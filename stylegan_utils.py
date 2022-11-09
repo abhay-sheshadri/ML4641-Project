2a7be9b0afdd0782082ab65503d215987a34b117
Abhay Sheshadri <abhay.sheshadri@outlook.com>
2022-11-09 13:10:14 -0500

    Added training code, modified stylegan

--- stylegan_utils.py
+++ stylegan_utils.py
@@ -2,40 +2,27 @@ import pickle
 import torch
 from PIL import Image
 import numpy as np
-from networks_stylegan3 import Generator
-from sklearn.cluster import KMeans
 
 
-def load_stylegan_generator(file_path, return_raw=False, requires_grad=False):
+def load_stylegan_generator(file_path, requires_grad=False):
     with open(file_path, 'rb') as f:
         G_temp = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
-        if return_raw:
-            return G_temp
-    G = Generator(
-        G_temp.z_dim,
-        G_temp.c_dim,
-        G_temp.w_dim,
-        G_temp.img_resolution,
-        G_temp.img_channels,
-        channel_base=16384).cuda()
-    G.load_state_dict(G_temp.state_dict())
-    for p in G.parameters():
-        p.requires_grad = requires_grad
-    return G
+        return G_temp
 
 
-G = load_stylegan_generator("anime.pkl", return_raw=True)
-for p in G.parameters():
-    p.requires_grad = True
+G = load_stylegan_generator("anime.pkl")
 G.eval()
+
 
 def generate_random_w(n=1):
     z = torch.randn([n, G.z_dim]).cuda()
-    w = G.mapping(z, None, truncation_psi= 0.7, truncation_cutoff=8)
+    w = G.mapping(z, None, truncation_psi=0.7, truncation_cutoff=8)
     return w
 
+
 def convert_torch_to_pil(image_tensor):
-    img = (image_tensor.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    img = (image_tensor.permute(0, 2, 3, 1) *
+           127.5 + 128).clamp(0, 255).to(torch.uint8)
     images = []
     for i in range(img.shape[0]):
         if img.shape[-1] == 3:
@@ -56,42 +43,8 @@ def generate_image(w=None, coords=[0, 0], scale=1, as_pil=True):
         m[1][1] = scale
         m = np.linalg.inv(m)
         G.synthesis.input.transform.copy_(torch.from_numpy(m))
-    img, maps = G.synthesis(w, noise_mode="const", return_maps=True)
-    for i in range(13):
-        maps[i] = maps[i][:, :, 10:-10, 10:-10].to(torch.float32)
-    maps[-2] = maps[-2].to(torch.float32)
+    img = G.synthesis(w, noise_mode="const",)
     if as_pil:
-        return convert_torch_to_pil(img), maps[:-1]
+        return convert_torch_to_pil(img)
     else:
-        return img, maps[:-1]
-
-
-def clustering_segmentation_maps(map_layer=11):
-
-    X = []
-    for i in range(25):
-        im, maps = generate_image()
-        X.append(
-            maps[map_layer].cpu().detach().permute(0, 2, 3, 1).squeeze(0).numpy()
-        )
-        
-    X = np.stack(X)
-    X = X.reshape(-1, X.shape[-1])
-    print("Processing", X.shape[0], "vectors...")
-    kmeans = KMeans(n_clusters=10, random_state=0).fit(X)
-
-    colors = [tuple(np.random.choice(range(256), size=3)) for i in range(10)]
-
-    for k in range(5):
-        img1, maps = generate_image()
-        img2 = Image.new("RGB", (maps[map_layer].shape[2], maps[map_layer].shape[3]))
-        pixels = img2.load()
-        for i in range(maps[map_layer].shape[2]):
-            for j in range(maps[map_layer].shape[3]):
-                vec = maps[map_layer][:, :, i, j].cpu().detach().numpy()
-                seg = kmeans.predict(vec)[0]
-                pixels[j, i] = colors[seg]
-        
-        img1.save(f"raw_{k}.png")
-        img2.save(f"seg_{k}.png")
-
+        return img
--- train.py
+++ train.py
@@ -4,35 +4,89 @@ from datasets import AnimeDataset
 
 import torch
 from torch import nn, optim
+import torch.nn.functional as F
 from torch.utils.data.sampler import SubsetRandomSampler
 import glob
 import os
 
 hyperparams = {
-    "num_epochs": 100,
+    "num_epochs": 10,
     "batch_size": 32,
     "learning_rate": 0.001,
+    "resize": (32, 32),
 
-    "save_interval": 4096,
-    "lpips_lambda": 0.8,
-    "l2_lambda": 1,
-    "w_norm_lambda": 0,
-    "moco_lambda": 0.5,
+    "use_synthetic": True,
+    "num_batches": 10,
 
-    "input_nc": 6,
-    "n_iters_per_batch": 5,
-    "output_size": 256,
-    "n_styles": 2,
-    "n_layers": 18,
-
-    "load_prev": False
+    "num_test": 3
 }
 
 
-def main():
+def train_with_synthetic():
 
     # Initialize model
-    net = ResnetModel(3, 512)
+    net = ResnetModel(1, 512, n_layers=50, res=hyperparams["resize"]).cuda()
+    optimizer = torch.optim.AdamW(
+        net.parameters(), lr=hyperparams["learning_rate"])
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, hyperparams["num_epochs"])
+
+    for epoch in range(hyperparams["num_epochs"]):
+
+        net.train()
+        for batch_idx in range(hyperparams["num_batches"]):
+
+            optimizer.zero_grad()
+
+            # Sample images from Gan distribution
+            with torch.no_grad():
+                w = generate_random_w(16)
+                img = G.synthesis(w, noise_mode="const",)
+
+            # Downsample
+            img = F.interpolate(
+                img, hyperparams["resize"]).mean(1, keepdim=True)
+            pred_latent = net(img)
+            loss = F.mse_loss(pred_latent, w[:, 0, :])
+
+            # improve
+            loss.backward()
+            optimizer.step()
+
+        net.eval()
+        test_distances = []
+        with torch.no_grad():
+            for i in range(hyperparams["num_test"]):
+                # Generate an initial image to put through the network
+                w = generate_random_w(1)
+                img = G.synthesis(w, noise_mode="const",)
+                imgd = F.interpolate(
+                    img, hyperparams["resize"]).mean(1, keepdim=True)
+                pred_latent = net(imgd).unsqueeze(0).repeat(1, 16, 1)
+                imgd = F.interpolate(imgd, (256, 256)).repeat(1, 3, 1, 1)
+                # Add tests
+                images = torch.cat([img, imgd], dim=3)
+                for j in range(3):
+                    w2 = generate_random_w(1)
+                    pred_latent[0, 8:, :] = w2[0, 8:, :]
+                    imgr = G.synthesis(pred_latent, noise_mode="const",)
+                    images = torch.cat([images, imgr], dim=3)
+
+                images = images.detach().cpu()
+                convert_torch_to_pil(images)[0].save(f"test_{i}.jpg")
+                test_distances.append(F.mse_loss(
+                    w[0, 0, :], pred_latent[0, 0, :]).item())
+
+        print(
+            f"Epoch: {epoch+1}, MSE Latent Loss: {sum(test_distances) / len(test_distances)}")
+        torch.save(net.state_dict(), "weights.pkl")
+        scheduler.step()
+
+
+def train_with_real():
+
+    # Initialize model
+    net = ResnetModel(3, 512, res=hyperparams["resize"])
     optimizer = torch.optim.AdamW(
         net.parameters(), lr=hyperparams["learning_rate"])
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
@@ -58,12 +112,16 @@ def main():
         batch_size=hyperparams["batch_size"],)
 
     # Training loop
-    
+
     for epoch in range(hyperparams["num_epochs"]):
 
         for idx, gt_images in enumerate(train_loader):
             gt_images = gt_images.cuda()
+            rs_images = F.interpolate(gt_images, hyperparams["resize"])
 
 
 if __name__ == "__main__":
-    main()
+    if hyperparams["use_synthetic"]:
+        train_with_synthetic()
+    else:
+        train_with_real()
